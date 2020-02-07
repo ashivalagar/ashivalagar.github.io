ef9207a0338dbfe0360f7cd4f38aa9b98332b06d
A Shiv Alagar <ashivalagar@gmail.com>
2020-02-07 15:40:33 +0800

    Updates

---
+++ precache-manifest.7e4864f74fd68a2eb32cd0fcdc373435.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6ef86a850d203bd668b4fdf84a1ba17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9efae2ffc3ec9fd2c4b",
+    "url": "/static/css/2.1e105a3b.chunk.css"
+  },
+  {
+    "revision": "bd3c993ac55f8c9ebf4c",
+    "url": "/static/css/main.3ad4ed62.chunk.css"
+  },
+  {
+    "revision": "c9efae2ffc3ec9fd2c4b",
+    "url": "/static/js/2.378dec35.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.378dec35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd3c993ac55f8c9ebf4c",
+    "url": "/static/js/main.36046676.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "e0052efb7c45925c57bd13b5095eb4ed",
+    "url": "/static/media/Cover.e0052efb.jpg"
+  }
+]);
