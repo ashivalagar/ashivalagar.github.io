6d722ce1383f66ac25caa604edbd91d2bc8502dd
Tgh Ashok Kumar <stghasho@amd.com>
2020-02-07 16:25:57 +0800

    Update landing page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,7 +5,7 @@ const antIcon = <Icon type="loading" style={{ fontSize: 24 }} spin />;
 
 const Home = () => {
     return (
-        <div style={{ paddingTop: "40%" }}>
+        <div style={{ paddingTop: "20%" }}>
             <Row type="flex" justify="center" align="middle">
                 <Col>
                     <h3>Coming Soon....</h3>
