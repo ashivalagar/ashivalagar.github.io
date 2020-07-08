c2d7f0d6343e3036c53b4947b401ab6ef881f67e
A Shiv Alagar <ashivalagar@gmail.com>
2020-07-08 23:29:18 +0800

    Leggo

--- client/src/pages/Project.js
+++ client/src/pages/Project.js
@@ -1,42 +1,40 @@
-import React from 'react';
-import fetch from 'node-fetch';
+import React from "react";
+import fetch from "node-fetch";
 
 //TODO parallax scroll
 
 class Project extends React.Component {
-    state = {
-        projects: []
-    }
-    async componentDidMount() {
-        let repos = []
-        // fetch("https://api.github.com/users/ashivalagar/repos").then(res => res.json()).then(res => console.log(res));
-        let apiCall = await fetch("https://api.github.com/users/ashivalagar/repos")
-        let apiRes = await apiCall.json();
-        apiRes.forEach(element => {
-            repos.push({repoName: element.name, repoUrl: element.html_url})
-        });
-        this.setState({projects: repos})
-    }
-    render(){
-        return(
-            <div>
-                <ul>
-                            {
-
-                                this.state.projects.map(i => {
-                                    return (<li><a href={i.repoUrl}>
-                                        <p>
-                                            {i.repoName}
-                                            </p>
-                                            </a>
-                                    </li>
-                                    );
-                                })
-                            }
-                        </ul>
-            </div>
-        );
-    }
+  state = {
+    projects: [],
+  };
+  async componentDidMount() {
+    let repos = [];
+    // fetch("https://api.github.com/users/ashivalagar/repos").then(res => res.json()).then(res => console.log(res));
+    let apiCall = await fetch("https://api.github.com/users/ashivalagar/repos");
+    let apiRes = await apiCall.json();
+    apiRes.forEach((element) => {
+      repos.push({ repoName: element.name, repoUrl: element.html_url });
+    });
+    this.setState({ projects: repos });
+  }
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.state.projects.map((i) => {
+            return (
+              <li>
+                <a href={i.repoUrl}>
+                  <p>{i.repoName}</p>
+                  <div>Yo</div>
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default Project;
