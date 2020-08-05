9c3b0272518c833f0e35fa270c654ce9389d6384
A Shiv Alagar <ashivalagar@gmail.com>
2020-08-05 23:36:01 +0800

    Updates

---
+++ precache-manifest.229cca9c6b3b37fdb7b27b8e8e601653.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "351d03a3184bc1b1b5bf94c97bb864c2",
+    "url": "/client/index.html"
+  },
+  {
+    "revision": "07d40bb8fcb9797761d9",
+    "url": "/client/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fc03aed87d75d818f89c",
+    "url": "/client/static/js/2.09f1b02a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/client/static/js/2.09f1b02a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07d40bb8fcb9797761d9",
+    "url": "/client/static/js/main.e5278cf4.chunk.js"
+  },
+  {
+    "revision": "d64b09180f5230249a46",
+    "url": "/client/static/js/runtime-main.1d4c076d.js"
+  }
+]);
